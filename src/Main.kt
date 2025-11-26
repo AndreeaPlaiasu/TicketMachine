75878208a58aa3c337041fa001505d53a8096641
nick <gavrilutageoge@gmail.com>
2025-11-26 19:28:18 +0000

    added second menu

--- src/Main.kt
+++ src/Main.kt
@@ -13,6 +13,19 @@ fun main() {
             "2" -> {
                 print("Username: "); val u = sc.nextLine()
                 print("Password: "); val p = sc.nextLine()
+                if(login.login(u, p)){
+                    while (login.isLoggedIn){
+                        println("\n1. View Offers 2. Add Offer 3. Search 4. Delete")
+                        println("Choice:")
+                        when(sc.nextLine()){
+                            "1" -> offers.viewOffers()
+                            "2" ->{
+                                machine.ListDestinations()
+                            }
+
+                        }
+                    }
+                }
             }
 
         }
