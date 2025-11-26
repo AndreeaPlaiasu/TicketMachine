891c6dd4a0c52cc40b637d68d6fb21db19cafbc5
nick <gavrilutageoge@gmail.com>
2025-11-26 19:38:21 +0000

    added special offers

--- src/Main.kt
+++ src/Main.kt
@@ -4,30 +4,4 @@ fun main() {
     ui.start()
     val login = LoginSystem()
 
-    while (true){
-        println("\n1. Buy ticket 2. Admin Login 3. Exit")
-        print("Choice:")
-
-        when(sc.nextLine()){
-            "1" -> ui.start()
-            "2" -> {
-                print("Username: "); val u = sc.nextLine()
-                print("Password: "); val p = sc.nextLine()
-                if(login.login(u, p)){
-                    while (login.isLoggedIn){
-                        println("\n1. View Offers 2. Add Offer 3. Search 4. Delete")
-                        println("Choice:")
-                        when(sc.nextLine()){
-                            "1" -> offers.viewOffers()
-                            "2" ->{
-                                machine.ListDestinations()
-                            }
-
-                        }
-                    }
-                }
-            }
-
-        }
-    }
 }
