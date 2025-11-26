ea3c4f8ac0af8b18f8232acc04d410a226ae8167
nick <gavrilutageoge@gmail.com>
2025-11-26 19:17:52 +0000

    menu

--- src/Main.kt
+++ src/Main.kt
@@ -3,4 +3,18 @@ fun main() {
     val ui = UserInterface(machine)
     ui.start()
     val login = LoginSystem()
+
+    while (true){
+        println("\n1. Buy ticket 2. Admin Login 3. Exit")
+        print("Choice:")
+
+        when(sc.nextLine()){
+            "1" -> ui.start()
+            "2" -> {
+                print("Username: "); val u = sc.nextLine()
+                print("Password: "); val p = sc.nextLine()
+            }
+
+        }
+    }
 }
