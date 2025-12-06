b8d675c001a0274f4fc6c849b1a114ff08e1e833
AndreeaPlaiasu <0plaia32@solent.ac.uk>
2025-12-06 10:16:09 +0000

    Add files via upload

--- Main.kt
+++ Main.kt
@@ -1,12 +1,5 @@
-package ticketmachine
-
 fun main() {
-    println("=== Ticket Machine ===")
-    
-    val catalog = CatalogRepository()
-    val pricing = PricingService()
-    val origin = catalog.stations[0] // Assuming the first station is the starting point
-    val ticketMachine = TicketMachine(catalog, pricing, origin)
-
-    ticketMachine.showDestinations()
+    val machine = TicketMachine()
+    val ui = UserInterface(machine)
+    ui.start()
 }
